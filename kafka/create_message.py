def4e21d16fc3e674d6512f36e48ca084143cd8e
qintao <670026955@qq.com>
2023-09-27 22:46:28 +0800

    kafka 配置

--- kafka/create_message.py
+++ kafka/create_message.py
@@ -3,6 +3,7 @@ import os
 
 #9092:32452/TCP
 producer = KafkaProducer(bootstrap_servers=os.environ.get('KAFKA_HOST', 'node1:32452'))
+print("------->>")
 future = producer.send('test', key=b'my_key', value=b'my_value', partition=0)
 result = future.get(timeout=100)
 print(result)
