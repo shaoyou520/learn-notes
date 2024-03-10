fa0f0ad592c9aaab8435ce906a36253694f0627a
qintao <670026955@qq.com>
2024-03-10 16:15:30 +0800

    公网安装k8s

--- canal/create_date.py
+++ canal/create_date.py
@@ -5,22 +5,22 @@ conn = pymysql.connect(
     host="node1", port=32306, user="syblog", password="Abcde.123", database="blog", charset="utf8"
 )
 
-insert_sql = "insert into blog_base(title) values ('canal')"
-update_sql = "update blog_base set title = 'canal11' where title = 'canal'"
-delete_sql = "delete from blog_base"
+insert_sql = "insert into test(user_name, age) values ('canal', 12)"
+# update_sql = "update blog_base set title = 'canal11' where title = 'canal'"
+# delete_sql = "delete from blog_base"
 
 cursor = conn.cursor()
 num = 0
 while True:
     num += 1
     print("---->", num)
-    time.sleep(1)
+    time.sleep(10)
     cursor.execute(insert_sql)
     conn.commit()
-    time.sleep(1)
-    cursor.execute(update_sql)
-    conn.commit()
-    time.sleep(1)
-    cursor.execute(delete_sql)
-    conn.commit()
-    time.sleep(1)
+    # time.sleep(1)
+    # cursor.execute(update_sql)
+    # conn.commit()
+    # time.sleep(1)
+    # cursor.execute(delete_sql)
+    # conn.commit()
+    # time.sleep(1)
